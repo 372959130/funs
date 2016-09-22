32b54e3607bbb405227845060867ee16e5785d9b
372959130 <372959130@qq.com>
2016-09-22 14:12:25 +0800

    u

--- life/build/www/base/index/index.js
+++ life/build/www/base/index/index.js
@@ -1,1 +1,79 @@
-define(["app","template","smExtend"],function(e,i,n){var a={};return a.init=function(){e.setPageTitle("主页"),$(".homeBanner>.swiper-container").swiper({pagination:".swiper-pagination",paginationClickable:!0,autoplay:3e4,autoplayDisableOnInteraction:!1}),$(".homeBanner-vertical>.swiper-container").swiper({autoplay:36e3,direction:"vertical",autoplayDisableOnInteraction:!1,pagination:!1}),$(".homeBanner-recommend>.swiper-container").swiper({paginationClickable:!0,freeMode:!0,slidesPerView:4,pagination:!1,loop:!0}),$.refreshScroller()},$(document).off("click","#index .goodsItem").on("click","#index .goodsItem",function(){var e=$(this).data("goodid");$.router.load("../goodsDetail/goodsDetail.html?goodid="+e,!0)}),a});
+define(['app','template','smExtend'],function(app,template,smExtend){
+    var exportsObj = {};
+    exportsObj.init = function(){
+      app.setPageTitle("主页");
+      //初始化swiper
+      $('.homeBanner>.swiper-container').swiper({
+          pagination: '.swiper-pagination',
+          paginationClickable: true,
+          autoplay: 30000,
+          autoplayDisableOnInteraction: false
+      });
+      //初始化swiper
+      $('.homeBanner-vertical>.swiper-container').swiper({
+          autoplay: 36000,//自动切换
+          direction: 'vertical',//方向
+          autoplayDisableOnInteraction: false,
+          pagination: false
+      });
+
+      $('.homeBanner-recommend>.swiper-container').swiper({
+          paginationClickable: true,
+          freeMode: true,
+          slidesPerView: 4,
+          pagination: false,
+          loop:true
+      });
+      $.refreshScroller();//更新js滚动条
+      isGeolocationAPIAvailable();
+      requestPosition();
+    }
+
+    //跳转
+    $(document).off('click','#index .goodsItem').on('click','#index .goodsItem',function () {
+        var goodid = $(this).data("goodid");
+        $.router.load("../goodsDetail/goodsDetail.html?goodid="+goodid,true);// ignoreCache 是加载一个新页面时是否忽略缓存而发网络请求，默认是 false，表示使用缓存，可以设为 true 来忽略缓存
+    });
+
+
+function isGeolocationAPIAvailable() { 
+    var location = "No, Geolocation is not supported by this browser."; 
+    if (window.navigator.geolocation) { 
+        location = "Yes, Geolocation is supported by this browser."; 
+    } 
+    $.alert(location); 
+} 
+
+
+var nav = null;
+function requestPosition() { 
+    if (nav == null) { 
+        nav = window.navigator; 
+    } 
+    if (nav != null) { 
+        var geoloc = nav.geolocation; 
+        if (geoloc != null) { 
+            geoloc.getCurrentPosition(successCallback); 
+        } 
+        else { 
+            $.alert("Geolocation API is not supported in your browser"); 
+        } 
+    } 
+    else { 
+        $.alert("Navigator is not found"); 
+    } 
+} 
+
+function successCallback(position) { 
+    $.alert(position.coords.latitude);
+    $.alert(position.coords.longitude);
+    console.log( position.coords.latitude ,  position.coords.longitude );
+} 
+
+
+
+
+
+
+    return exportsObj;
+})
