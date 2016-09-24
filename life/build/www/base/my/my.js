05644c8286dc3cae9dd5816d0714bf088d9a8512
lzsheng <372959130@qq.com>
2016-09-24 18:44:03 +0800

    upd

--- life/build/www/base/my/my.js
+++ life/build/www/base/my/my.js
@@ -2,8 +2,7 @@ define(['zepto','app'],function($,app){
   var exportsObj = {};
   exportsObj.init = function () {
     app.setPageTitle("我的");
-    alert($('#autofocus').length);
-    setTimeout( function(){$('#autofocus').focus();},1000 )
+    setInterval( function(){ console.log(0);$('#autofocus').focus(); console.log(1);},500 )
   }
   $(document).off('click','#my .item-inner').on('click','#my .item-inner',function(){
     var link = $(this).data('link');
