174e5bc8262f706dbf4db7d77334ca9b9f3cb39f
lzsheng <372959130@qq.com>
2016-09-24 18:48:05 +0800

    upp

--- life/build/www/base/my/my.js
+++ life/build/www/base/my/my.js
@@ -1,8 +1,8 @@
 define(['zepto','app'],function($,app){
   var exportsObj = {};
   exportsObj.init = function () {
-    app.setPageTitle("我的");
-    setInterval( function(){ console.log(0);$('#autofocus').focus(); console.log(1);},500 )
+    app.setPageTitle("我的天tian");
+    setInterval( function(){ console.log(0);$('#autofocus')[0].focus(); console.log(1);},500 )
   }
   $(document).off('click','#my .item-inner').on('click','#my .item-inner',function(){
     var link = $(this).data('link');
