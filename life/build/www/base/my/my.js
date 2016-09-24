f2ab68db660e2bbdd174362186764cf7a55ec54f
lzsheng <372959130@qq.com>
2016-09-24 18:22:03 +0800

    up

--- life/build/www/base/my/my.js
+++ life/build/www/base/my/my.js
@@ -2,6 +2,7 @@ define(['zepto','app'],function($,app){
   var exportsObj = {};
   exportsObj.init = function () {
     app.setPageTitle("我的");
+    alert($('#testautofocusfocus').length);
     setTimeout( function(){$('#testautofocusfocus').focus();},1000 )
   }
   $(document).off('click','#my .item-inner').on('click','#my .item-inner',function(){
