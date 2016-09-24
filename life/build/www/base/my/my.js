d38ef091f7e256f8c39a2d145f71e63ec973cbe4
lzsheng <372959130@qq.com>
2016-09-24 18:14:56 +0800

    uu

--- life/build/www/base/my/my.js
+++ life/build/www/base/my/my.js
@@ -2,9 +2,7 @@ define(['zepto','app'],function($,app){
   var exportsObj = {};
   exportsObj.init = function () {
     app.setPageTitle("我的");
-    setTimeout(funciton(){
-      $('#testautofocusfocus').focus();
-    },1500);
+    setTimeout( function(){$('#testautofocusfocus').focus();},1000 )
   }
   $(document).off('click','#my .item-inner').on('click','#my .item-inner',function(){
     var link = $(this).data('link');
