0b0567e10593972541dbe5298aac1415aa80d5fb
372959130 <372959130@qq.com>
2016-09-26 12:03:30 +0800

    update

---
+++ life/build/www/base/goods/goodsDetail.js
@@ -0,0 +1,22 @@
+define(['app','template','smExtend'],function(app,template,smExtend){
+    var exportsObj = {};
+    exportsObj.init = function(){
+      app.setPageTitle("美食");
+      //初始化swiper
+      $('.homeBanner>.swiper-container').swiper({
+          pagination: '.swiper-pagination',
+          paginationClickable: true,
+          autoplay: 30000,
+          autoplayDisableOnInteraction: false
+      });
+      $.refreshScroller();//更新js滚动条
+    }
+
+    //跳转
+    $(document).off('click','#index .goodsItem').on('click','#index .goodsItem',function () {
+        var goodid = $(this).data("goodid");
+        $.router.load("../goodsDetail/goodsDetail.html?goodid="+goodid,true);// ignoreCache 是加载一个新页面时是否忽略缓存而发网络请求，默认是 false，表示使用缓存，可以设为 true 来忽略缓存
+    });
+
+    return exportsObj;
+})
